2d217b0a997079bbd4da9e172ab4fc8a922f3f0e
Alfonso Vargas Mesa <vargasmesa.alfonso@gmail.com>
2021-08-15 18:47:29 +0200

    Moongose and dotenv installed - added function to connect to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 //modules
+const dotenv = require('dotenv').config();
 const express = require('express');     // const http = require('http');
+const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 
 //variables
-const PORT = process.env.PORT || 3001;
+const APP_PORT = process.env.APP_PORT || 3001;
 const HOST = 'localhost';
+
+function mongoConnect() {
+    mongoose.connect(process.env.DB_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)}
+
+// mongoConnect();
 
 app.use(express.static(__dirname));
 
@@ -14,6 +25,7 @@ app.get('/', (req, res) => {
     // res.send('<h1>Hello World</h1>');
 });
 
-app.listen(PORT);
-// console.log('Server is running on ' + PORT);
-console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(APP_PORT);
+// console.log('Server is running on ' + APP_PORT);
+console.log(`Server is running on http://${HOST}:${APP_PORT}`);
+console.log('The value of APP_PORT is: ' + process.env.APP_PORT);
